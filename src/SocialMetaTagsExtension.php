b95167dc936a9bfa3c4168a77039816e54831dae
kevin-hine-innis <83420315+kevin-hine-innis@users.noreply.github.com>
2021-09-15 19:22:53 -0400

    Update SocialMetaTagsExtension.php
    Don't add additional fields to error pages.

--- src/SocialMetaTagsExtension.php
+++ src/SocialMetaTagsExtension.php
@@ -30,7 +30,7 @@ class SocialMetaTagsExtension extends DataExtension {
     public function updateCMSFields(FieldList $fields) {
         $owner = $this->getOwner();
 
-        if (is_subclass_of($owner, SiteTree::class) && !is_a($owner, RedirectorPage::class)) {
+        if (is_subclass_of($owner, SiteTree::class) && !is_a($owner, RedirectorPage::class) && !is_a($owner, ErrorPage::class)) {
             $fields->addFieldToTab('Root.Main.Metadata', TextareaField::create("SocialMetaDescription")->addExtraClass("stacked"));
             $fields->addFieldToTab('Root.Main.Metadata', UploadField::create("SocialMetaImage")->addExtraClass("stacked"));
         }
